dee5d158f8313f7cb0d9b95791dee40f32cebe2b
Othmane Ataallah <othmaneataallah123@gmail.com>
2025-02-20 10:56:41 +0100

    First Shaders

--- src/main/java/me/oataallah/jade/KeyListener.java
+++ src/main/java/me/oataallah/jade/KeyListener.java
@@ -7,7 +7,7 @@ public class KeyListener {
 
     private static KeyListener instance;
 
-    private boolean keyPressed[] = new boolean[350];
+    private final boolean[] keyPressed = new boolean[350];
 
     private KeyListener() {
     }
